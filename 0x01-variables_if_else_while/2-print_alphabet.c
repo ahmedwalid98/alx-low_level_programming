f8121dbeecc32181197bfdbbc7dfc1b3ed672f85
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-17 18:37:38 +0000

    varaibles and if else and while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
