364e6526a6f0cd9bc6d9dcd90b30747dcb45860e
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-17 22:01:05 +0000

    advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (x = j + 1; x < 10; x++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				if (j == 8 && i == 7 && x == 9)
+					continue;
+				putchar(44);
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
