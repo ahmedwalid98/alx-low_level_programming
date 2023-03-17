54d2fd365c51ec433df3d34ebd0a2e1b861deabe
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-17 19:04:50 +0000

    varaibles and if else and while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *  main -Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
