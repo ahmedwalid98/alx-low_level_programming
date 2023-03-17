2b8cfa885ef89e60b8919ab56f02c2892f64a9aa
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-17 18:13:33 +0000

    varaibles and if else and while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,10 @@
 /* more headers goes there */
 #include <stdio.h>
 /* betty style doc for function main goes there */
+/*
+ * main - entry point
+ * return always 0
+ * /
 int main(void)
 {
 	int n;
