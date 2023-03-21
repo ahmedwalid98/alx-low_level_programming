295827eb6df5b785227f99df07d88dd7b3233418
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-21 17:09:04 +0000

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <cstdlib>
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The number to get absoluted
+ * Return: return the absolute number
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
