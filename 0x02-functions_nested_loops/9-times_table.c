84c6639c1fe5d3ede111fad6c012b95d060283e0
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-21 18:36:07 +0000

    functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,14 +4,12 @@
  */
 void times_table(void)
 {
-		int i = 0;
-
-		while (i <= 9)
-		{
-			_putchar('0' + 0);
-			_putchar(',');
-			_putchar(' ');
-		int j = 1;
+	while (i <= 9)
+	{
+		_putchar('0' + 0);
+		_putchar(',');
+		_putchar(' ');
+		int j =1;
 
 		while (j <= 9)
 		{
