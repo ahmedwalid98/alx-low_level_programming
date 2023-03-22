1ee5a0bb77fee9e039cbf029936bdb38b86d9a83
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-22 12:51:05 +0000

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * positive_or_negative - Test if it gives the right output
+ * @i: Number to test
+ */
+void positive_or_negative(int n)
+{
+	if (n == 0)
+	{
+		printf("%d is zero/n", n);
+	}
+}
