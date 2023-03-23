576fd7c089e1f102b86aa0f73dffd9ff2e207855
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-23 12:58:48 +0000

    more funtions

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - Make square shape
+ * @size: Number of rows and columns
+ */
+void print_square(int size)
+{
+	if (size == 0 || size < 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+	}
+}
