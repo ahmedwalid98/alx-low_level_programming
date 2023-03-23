0a0405ede1757e7d14ad8472c45d7376d98b8c97
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-23 21:44:44 +0000

    more funtions

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * print_number - Print all the integer
+ * @n: Number to be printed
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		putchar(0 + '0');
+	} else
+	{
+		int tmp = n, i = 1;
+
+		while (tmp != 0)
+		{
+			i *= 10;
+			tmp /= 10;
+		}
+		i = i / 10;
+		if (n < 0)
+		{
+			n *= -1;
+			putchar('-');
+		}
+		while (i != 0)
+		{
+			putchar(n / i + '0');
+			n = n % i;
+			i = i / 10;
+		}
+	}
+}
