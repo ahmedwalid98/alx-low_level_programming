5f864595b8a2f33ff7721d20e23a4a553eb1fe41
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-27 23:32:14 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - Copies string pointed by src
+ * @src: The source string
+ * @dest: The copied string
+ * Return: Destination string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+}
