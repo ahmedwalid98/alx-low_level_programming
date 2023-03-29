95107c3b18d9adb479514fefa9d2679c711b051d
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-29 15:25:51 +0000

    More pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - Copies string
+ * @dest: The final result
+ * @src: the string to be copied
+ * @n: Length of string
+ * Return: A string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *s = dest;
+	int i = 0;
+
+	while (i < n && *dest != '\0')
+	{
+		*dest = *src;
+		*dest++;
+		*src++;
+		i++;
+	}
+	return (s);
+}
+
