3404edaaf2ad3188889458e100e0b05adf71452e
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-30 18:14:40 +0000

    More pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,28 +6,36 @@
  */
 char *cap_string(char *s)
 {
-	char *tmp = s;
 	int i = 0;
 
-	while (*s != '\0')
+	/*iterate through our array values*/
+	while (s[i] != '\0')
 	{
-		if (i == 0 && (*s >= 'a' && *s <= 'z'))
+		/*check for any lowercase letters*/
+		if (s[i] >= 97 && s[i] <= 122)
 		{
-			*s -= 'a' - 'A';
-			i++;
-		}
-		if (*s == ',' || *s == ';' || *s == '.' || *s == '!' || *s == '?'
-				|| *s == '"' || *s == '(' || *s == ')'
-				|| *s == '{' || *s == '}' || *s == ' '
-				|| *s == '\t' || *s == '\n')
-		{
-			if (*(s + 1) >= 'a' && *(s + 1) <= 'z')
+			/**
+			 * if we have a null character
+			 * change its value to capital
+			*/
+			if (i == 0)
 			{
-				*(s + 1) -= 'a' - 'A';
-				s += 2;
+				s[i] -= 32;
+			}
+			/**
+			 * if we find any character matching the below before any small
+			 * letter we change that value to a capital letter.
+			*/
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+				s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+				s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+				s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+				s[i - 1] == 124)
+			{
+				s[i] -= 32;
 			}
 		}
-		s++;
+		i++;
 	}
-	return (tmp);
+	return (s);
 }
