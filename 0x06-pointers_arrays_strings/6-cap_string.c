0021552763b85012e5fc6dd3a8ee05609dbde2ea
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-30 18:04:46 +0000

    More pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: The sentence
+ * Return: The final string
+ */
+char *cap_string(char *s)
+{
+	char *tmp = s;
+
+	while (*s != '\0')
+	{
+		if (*s == ',' || *s == ';' || *s == '.' || *s == '!' || *s == '?'
+				|| *s == '"' || *s == '(' || *s == ')'
+				|| *s == '{' || *s == '}' || *s == ' '
+				|| *s == '\t' || *s == '\n')
+		{
+			if (*(s + 1) >= 'a' && *(s + 1) <= 'z')
+			{
+				*(s + 1) -= 'a' - 'A';
+				s += 2;
+			}
+		}
+		s++;
+	}
+	return (tmp);
+}
