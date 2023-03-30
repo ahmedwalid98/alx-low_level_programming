79f884fa3463ecf85ee8e3acf9d2fc3443a03e0c
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-03-30 17:38:12 +0000

    More pointers

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - Changes all lowercase to uppercase
+ * @s: The string
+ * Return: The new string
+ */
+char *string_toupper(char *s)
+{
+	while (*s != '\0')
+	{
+		if (*s >= 'a' || *s <= 'z')
+		{
+			*s -= 'a' - 'A';
+		}
+		s++;
+	}
+	return (s);
+}
