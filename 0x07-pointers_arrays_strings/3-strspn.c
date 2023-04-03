d1bab93a12ef0cd50bbd43f3a680938ef5beb3f8
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-03 22:59:06 +0000

    more pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,19 +7,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j = 0;
+	unsigned int i, j;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		while (accept[j] != s[i])
+		for (j = 0; accept[j] != s[i]; j++)
 		{
-			j++;
 			if (accept[j] == '\0')
 			{
 				return (i);
 			}
 		}
-		i++;
 	}
 	return (i);
 }
