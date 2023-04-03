7c86ee2a8e4c577d01848efb90fa367458c5325c
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-03 22:39:20 +0000

    more pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - Locates character in a string
+ * @s: The string
+ * @c: The character to be located
+ * Return: String started from located char
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
