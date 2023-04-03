e135ba3ebde94c97c89de31f997703bfb05dc5a1
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-03 23:34:42 +0000

    more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strstr - Loacates substring
+ * @haystack: First string to be searched
+ * @needle: Substring to be found
+ * Return: A pointer to sub string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j] && needle[0] == haystack[i])
+		{
+			if (haystack[i + 1] == needle[j])
+				j++;
+			else
+				break;
+		}
+		if (haystack[i])
+		{
+			i++;
+			j = 0;
+		} else
+		{
+			return (haystack + i);
+		}
+	}
+	return (0);
+}
