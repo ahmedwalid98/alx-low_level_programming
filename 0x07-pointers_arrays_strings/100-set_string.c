38a5609f72aa7f9848b9134450938bc1cb35c043
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-04 00:11:29 +0000

    more pointers

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
- * set_string - Sets the value of the pointer to char
- * @s: The pointer
- * @to: The value
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to change
+ * @to: string to change pointer to
+ *
+ * Return: void
  */
 void set_string(char **s, char *to)
 {
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,3 +5,4 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
