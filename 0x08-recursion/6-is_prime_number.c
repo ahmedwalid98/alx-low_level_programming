456a539c7eabd262b35d44848414fa5379f7eb47
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-06 00:37:57 +0000

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * check - checks for the square root
+ * @a:int
+ * @b:int
+ *
+ * Return: int
+ */
+int check(int a, int b)
+{
+        if (a == b)
+                return (a);
+        if (a > b)
+                return (0);
+        return (check(a + 2, b));
+}
+/**
+ * is_prime_number - Checks if the number is prime
+ * @n: The number to be checked
+ * Return: 1 if its prinme 0 if its not
+ */
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (0);
+	if (n == 2)
+		return (1);
+	return (check(3, n));
+}
