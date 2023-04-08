5fe3739020302787b0f78db9caba9434d8b65134
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-08 19:03:09 +0000

    arc and argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,8 +10,11 @@ int main(int argc, char **argv)
 {
 	char *f;
 
-	if (argc > 1)
+	if (argc < 2)
 	{
+		printf("0\n");
+		return (0);
+	}
 		for (i = 1; i < argc; i++)
 		{
 			n = strtol(argv[i], &f, 10);
@@ -19,15 +22,9 @@ int main(int argc, char **argv)
 			{
 				printf("Error\n");
 				return (1);
-			} else
-			{
-				sum += n;
 			}
+				sum += n;
 		}
 		printf("%d\n", sum);
-	} else
-	{
-		printf("0\n");
-	}
 	return (0);
 }
