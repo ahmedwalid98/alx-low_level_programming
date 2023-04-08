8d0d74b72678f6be9b08d63135f2dbf374b7c861
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-08 18:38:47 +0000

    arc and argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,11 @@
  */
 int main(int argc, char **argv)
 {
-	int mul = 0;
-
-	if (argc > 1)
+	if (argc < 3)
 	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		pritf("%d\n", mul);
-		return (0);
+		pritf("Error\n");
+		return (1);
 	}
-	printf("Error\n");
-	return (1);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]);
+	return (0);
 }
