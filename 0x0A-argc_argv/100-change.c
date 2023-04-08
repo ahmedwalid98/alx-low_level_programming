a9f1315009bb11de53280de3fa2d94360c15cd02
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-08 21:18:03 +0000

    arc and argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: arguments counter
+ * @argv: array of strings
+ * Return: always success (0)
+ */
+int main(int argc, char **argv)
+{
+	int i, j, sum = 0;
+	int changes[] = {25, 10, 5, 2, 1};
+
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		int cents = atoi(argv[i]);
+
+		while (cents > 0)
+		{
+			if (cents >= changes[j])
+			{
+				cents -= changes[j];
+				sum++;
+			} else
+			{
+				j++;
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
