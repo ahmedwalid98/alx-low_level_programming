ce4fa2f2e6062d6f4e24757ef134a90194f528c1
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-10 00:25:35 +0000

    fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,27 +8,29 @@
  */
 int main(int argc, char **argv)
 {
-	int j = 0, sum = 0;
-	int changes[] = {25, 10, 5, 2, 1};
-	int cents = atoi(argv[1]);
+	int cents, sum = 0;
 
-	if (argc == 1)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
+		cents = atoi(argv[1]);
 		while (cents > 0)
 		{
-			if (cents >= changes[j])
-			{
-				cents -= changes[j];
-				sum++;
-			} else
-			{
-				j++;
-			}
+			if (cents >= 25)
+				cents -= 25;
+			else if (cents >= 10)
+				cents -= 10;
+			else if (cents >= 5)
+				cents -= 5;
+			else if (cents >= 2)
+				cents -= 2;
+			else if (cents >= 1)
+				cents -= 1;
+			sum += 1;
 		}
 	}
 	printf("%d\n", sum);
