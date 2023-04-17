7c7554ddeeea4a779add27cbe9d0c1ca6c4147cb
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-17 14:26:04 +0000

    struct and typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "doh.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs
+ * @d: dog to be free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
