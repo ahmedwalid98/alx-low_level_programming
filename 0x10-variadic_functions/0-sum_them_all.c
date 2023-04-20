0426de4b40defb5335025f15ce6028303ce1f05b
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-04-20 15:24:57 +0000

    variadic

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sun_them_all - sum all of parameters
+ * @n: parameters
+ * Return: the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum = 0;
+	va_list nums;
+
+	va_start(nums, n);
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(nums, int);
+	}
+	va_end(nums);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef VARIADiC_FUNCTIONS_H
+#define CARIADIC_FUNCTIONS_H
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
