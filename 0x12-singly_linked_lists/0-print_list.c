8c57750939379b2eaa432783f033956c73cc72d7
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-04-27 23:12:17 +0000

    single linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,20 +6,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-
-	if (h == NULL)
-		return (0);
+	size_t s = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
-		{
+		if (!h->str)
 			printf("[0] (nil)\n");
-		}
-		printf("[%d] %s\n", h->len, h->str);
-		count++;
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
+		s++;
 	}
-	return (count);
+
+	return (s);
 }
