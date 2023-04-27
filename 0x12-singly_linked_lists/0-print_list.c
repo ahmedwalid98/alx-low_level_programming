129f8b46dde83e337cfaaa70e55f8d6654bbe420
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-04-27 23:01:30 +0000

    singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of the list
+ * @h: the list
+ * Return:  the size
+ */
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		printf("[%d] %s\n", h->size, h->str);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
