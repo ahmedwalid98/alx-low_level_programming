5d6f7281953bddf9f6bd19ef243ab731a4457687
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-05-01 22:00:10 +0000

    more singly linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - sum of all the data
+ * @head: the linked list
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
