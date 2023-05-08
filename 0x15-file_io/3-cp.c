96ad3545212dbd40f1962fdfe2b01885ab4b958d
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-05-08 18:15:55 +0000

    file io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ void check_98(ssize_t check, char* file, int fd_from, int fd_to)
 {
 	if (check == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE%s\n", file);
 		if (fd_from != -1)
 			close(fd_from);
 		if (fd_to != -1)
@@ -83,25 +83,25 @@ int main(int argc, char **argv)
 	char buffer[1024];
 	mode_t file_perm;
 
-	check97(argc);
+	check_97(argc);
 	fd_from = open(argv[1], O_RDONLY);
-	check98((ssize_t)fd_from, argv[1], -1, -1);
+	check_98((ssize_t)fd_from, argv[1], -1, -1);
 	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	check99((ssize_t)fd_to, argv[2], fd_from, -1);
+	check_99((ssize_t)fd_to, argv[2], fd_from, -1);
 	lenr = 1024;
 	while (lenr == 1024)
 	{
 		lenr = read(fd_from, buffer, 1024);
-		check98(lenr, argv[1], fd_from, fd_to);
+		check_98(lenr, argv[1], fd_from, fd_to);
 		lenw = write(fd_to, buffer, lenr);
 		if (lenw != lenr)
 			lenw = -1;
-		check99(lenw, argv[2], fd_from, fd_to);
+		check_99(lenw, argv[2], fd_from, fd_to);
 	}
 	close_to = close(fd_to);
 	close_from = close(fd_from);
-	check100(close_to, fd_to);
-	check100(close_from, fd_from);
+	check_100(close_to, fd_to);
+	check_100(close_from, fd_from);
 	return (0);
 }
