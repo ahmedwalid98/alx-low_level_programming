3f01761fd70b433ad2371889d2db946b0c43cf89
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-06-15 14:22:29 +0000

    file io

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len: get the list length
+ * @h: the list
+ * Return: the number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
