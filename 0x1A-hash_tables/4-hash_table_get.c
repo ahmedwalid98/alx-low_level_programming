d1d9fe95a1dd948154e823d577ea718a1ccab7a1
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-07-07 14:59:20 +0000

    hash tables

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,5 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 	new_node = ht->array[index];
+	while (new_node && strcmp(new_node->key, key) != 0)
+		new_node = new_node->next;
 	return ((new_node == NULL) ? NULL : new_node->value);
 }
